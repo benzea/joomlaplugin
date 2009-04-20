4c5a2e53800f6b5b7afd0a8d8626115f5b113a5e
Benjamin Berg <benjamin@sipsolutions.net>
2009-04-20 21:52:47 +0200

    Also accept the "Remember Me" cookie for authentication.
    This just accepts the "Remember Me" Cookie. It will *not* create a Joomla!
    session. The reason for this is that the host that this runs on has
    modifications which would make it completely unportable.

--- 0.11/joomla/session.py
+++ 0.11/joomla/session.py
@@ -8,7 +8,6 @@ class User(object):
 	username = None
 	guest = True
 	uid = 0
-	gid = 0
 	usertype = None
 	session_id = None
 
@@ -19,40 +18,12 @@ class JoomlaSession(Component):
 	live_site = Option("joomla", "live_site", default=None, doc="The Site to use for the cookie hash (defaults to the current hostname).")
 
 	def get_user(self, req):
-		db = JoomlaDatabaseManager(self.env)
-		cnx = db.get_connection()
-		cursor = cnx.cursor()
-		
-		table = db.get_table_name("session")
+	        user = self._get_user_from_session(req)
+	        if user is not None:
+	                return user
 
-		session_id = self._get_session_id(req)
-		if not session_id:
-			return None
-
-		sql = "SELECT username, guest, userid, usertype, gid FROM %s WHERE session_id=%%s AND guest=0 AND time >= (UNIX_TIMESTAMP()-%i);" \
-		       % (table, self.session_lifetime)
-		cursor.execute(sql, (session_id))
-		if cursor.rowcount > 1:
-			cnx.close()
-			raise AssertionError
-
-		if cursor.rowcount == 0:
-			cnx.close()
-			return None
-
-		user = User()
-
-		row = cursor.fetchone()
-		user.username = row[0]
-		user.guest = row[1]
-		user.uid = row[2]
-		user.usertype = row[3]
-		user.gid = row[4]
-		user.session_id = session_id
-
-		cnx.close()
-		
-		return user
+                # Try to create a joomla session from a "Remember Me" cookie
+                return self._create_session_from_remember_me(req)
 
 	def update_timestamp(self, user):
 		if user is None:
@@ -145,3 +116,129 @@ class JoomlaSession(Component):
 		else:
 			return None
 
+	def _get_user_from_session(self, req):
+		db = JoomlaDatabaseManager(self.env)
+		cnx = db.get_connection()
+		cursor = cnx.cursor()
+		
+		table = db.get_table_name("session")
+
+		session_id = self._get_session_id(req)
+		if not session_id:
+			return None
+
+		sql = "SELECT username, guest, userid, usertype FROM %s WHERE session_id=%%s AND guest=0 AND time >= (UNIX_TIMESTAMP()-%i);" \
+		       % (table, self.session_lifetime)
+		cursor.execute(sql, (session_id))
+		if cursor.rowcount > 1:
+			cnx.close()
+			raise AssertionError
+
+		if cursor.rowcount == 0:
+			cnx.close()
+			return None
+
+		user = User()
+
+		row = cursor.fetchone()
+		user.username = row[0]
+		user.guest = row[1]
+		user.uid = row[2]
+		user.usertype = row[3]
+		user.session_id = session_id
+
+		cnx.close()
+		
+		return user
+
+	def _get_remember_me_cookie_name(self, req):
+		if not self.live_site:
+			server_name = req.environ["HTTP_HOST"]
+		else:
+			server_name = self.live_site
+
+		hash = md5.md5()
+		hash.update("remembermecookieusername")
+		hash.update(self._get_cookie_name(req))
+
+		name_hash = md5.md5()
+		name_hash.update(self.hash_secret)
+		name_hash.update(hash.hexdigest())
+
+		return name_hash.hexdigest()
+
+	def _get_remember_me_cookie_value(self, req):
+		cookie_name = self._get_remember_me_cookie_name(req)
+
+		if req.incookie.has_key(cookie_name):
+			return req.incookie[cookie_name].value
+		else:
+			return None
+
+        def _create_session_from_remember_me(self, req):
+		db = JoomlaDatabaseManager(self.env)
+		cnx = db.get_connection()
+		cursor = cnx.cursor()
+
+		cookie = self._get_remember_me_cookie_value(req)
+		if cookie is None:
+			return None
+
+		# Cookie is too short
+		if len(cookie) < 65:
+			return None
+
+		cookie_userhash = cookie[0:32]
+		cookie_passhash = cookie[32:64]
+		cookie_uid = cookie[64:]
+
+
+		table = db.get_table_name("users")
+
+		sql = "SELECT id, username, password, usertype FROM %s WHERE id=%%s AND block=0;" \
+		       % (table,)
+		cursor.execute(sql, (cookie_uid,))
+
+		if cursor.rowcount > 1:
+			cnx.close()
+			raise AssertionError
+
+		if cursor.rowcount == 0:
+			cnx.close()
+			return None
+
+		user = User()
+
+		row = cursor.fetchone()
+		user.guest = 0
+		user.uid = row[0]
+		user.username = row[1]
+		passwd = row[2]
+		user.usertype = row[3]
+
+		cnx.close()
+
+		hash = md5.md5()
+		hash.update(req.environ["HTTP_USER_AGENT"])
+
+		salt = md5.md5()
+		salt.update(self.hash_secret)
+		salt.update(hash.hexdigest())
+		salt = salt.hexdigest()
+
+		userhash = md5.md5()
+		userhash.update(user.username)
+		userhash.update(salt)
+		userhash = userhash.hexdigest()
+
+		passwd = passwd.split(':')[0]
+		passhash = md5.md5()
+		passhash.update(passwd)
+		passhash.update(salt)
+		passhash = passhash.hexdigest()
+
+		# XXX: Just check that everything is valid, do NOT create a Joomla session!
+		if userhash == cookie_userhash and passhash == cookie_passhash:
+			return user
+		else:
+			return None
